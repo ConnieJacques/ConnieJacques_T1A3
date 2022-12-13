9dae8de4f1881e9777b4bc154c2d655e50f5d78f
ConnieJacques <connie.jacques91@gmail.com>
2022-12-13 14:55:01 +1100

    questions.py file updated to execute all questions as desired

--- src/questions.py
+++ src/questions.py
@@ -1,14 +1,13 @@
+import pyinputplus as pyip
 import csv
+from clear import clear
 
-# import pyinputplus
-# inputChoice() Ensures the user enters one of the provided choices
 
-# Dictionary stores the quizz game's questions
 questions_dictionary = {
     'Question One:': 'Which generation do you belong to?',
-    'Question Two:': 'Which musicians and bands were popular in the year you were born?',
-    'Question Four:': 'Which popular novel was released in the decade you were born?',
+    'Question Two:': 'Which popular novel was released in the decade you were born?',
     'Question Three:': 'Who was the Prime Minister of Australia in the year you were born?',
+    'Question Four:': 'Which musicians and bands were popular in the year you were born?',
     'Question Five:': 'What was the highest grossing film in the year you were born?'
 }
 
@@ -27,6 +26,7 @@ question_three = Questions(((list(questions_dictionary.keys())[2])), (list(quest
 question_four = Questions(((list(questions_dictionary.keys())[3])), (list(questions_dictionary.values())[3]))
 question_five = Questions(((list(questions_dictionary.keys())[4])), (list(questions_dictionary.values())[4]))
 
+
 # Access question.csv to read the questions and store in a list so they can be accessed via their indexes
 with open('question.csv') as f:
     reader = csv.DictReader(f)
@@ -38,7 +38,6 @@ with open('question.csv') as f:
 question_values = ['a)', 'b)', 'c)', 'd)', 'e)', 'f)', 'g)', 'h)', 'i)', 'j)', 'k)', 'l)', 'm)', 'n)', 'o)', 'p)', 'q)', 'r)', 's)', 't)']
 
 
-
 def question_series(question_code, question, answer_options):
     # Ask the first question
     print(question_code, question)
@@ -47,140 +46,315 @@ def question_series(question_code, question, answer_options):
         print(i, j)
 
 
-# assess user answer
-def answer_actions():
+# assess user answer is between letters a - g
+def answer_actions_ag():
     user_input = input()
-    # try:
-    if len(user_input) == 1 and user_input.lower().isalpha():
-        return user_input
-        # else:
-        #     print("Please answer questions by entering the corresponding letter and pressing ENTER.")
-    # except user_input == "quit":
-    #     raise KeyboardInterrupt
+    try:
+        if user_input.lower() == 'quit':
+            raise KeyboardInterrupt
+        elif len(user_input) == 1 and user_input.lower() in 'abcdefg':
+            return user_input
+        else: 
+            user_input = pyip.inputChoice(['a','b','c', 'd', 'e', 'f', 'g'])
+            return user_input
+    except KeyboardInterrupt:
+        print("It was nice while it lasted. See you next time!")
+
+
+# assess user's answer is between letters a - k
+def answer_actions_ak():
+    user_input = input()
+    try:
+        if user_input.lower() == 'quit':
+            raise KeyboardInterrupt
+        elif len(user_input) == 1 and user_input.lower() in 'abcdefghijk':
+            return user_input
+        else: 
+            user_input = pyip.inputChoice(['a','b','c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k'])
+            return user_input
+    except KeyboardInterrupt:
+        print("It was nice while it lasted. See you next time!")
+
+
+# assess user answer is between letters a - c
+def answer_actions_ac():
+    user_input = input()
+    try:
+        if user_input.lower() == 'quit':
+            raise KeyboardInterrupt
+        elif len(user_input) == 1 and user_input.lower() in 'abc':
+            return user_input
+        else: 
+            user_input = pyip.inputChoice(['a','b','c'])
+            return user_input
+    except KeyboardInterrupt:
+        print("It was nice while it lasted. See you next time!") 
+
+
+# assess user answer is between letters a - d
+def answer_actions_ad():
+    user_input = input()
+    try:
+        if user_input.lower() == 'quit':
+            raise KeyboardInterrupt
+        elif len(user_input) == 1 and user_input.lower() in 'abcd':
+            return user_input
+        else: 
+            user_input = pyip.inputChoice(['a','b','c', 'd'])
+            return user_input
+    except KeyboardInterrupt:
+        print("It was nice while it lasted. See you next time!")
+
+
+# assess user answer is between letters a - e
+def answer_actions_ae():
+    user_input = input()
+    try:
+        if user_input.lower() == 'quit':
+            raise KeyboardInterrupt
+        elif len(user_input) == 1 and user_input.lower() in 'abcde':
+            return user_input
+        else: 
+            user_input = pyip.inputChoice(['a','b','c', 'd', 'e'])
+            return user_input
+    except KeyboardInterrupt:
+        print("It was nice while it lasted. See you next time!")
+
+
+# assess user answer is between letters a - f
+def answer_actions_af():
+    user_input = input()
+    try:
+        if user_input.lower() == 'quit':
+            raise KeyboardInterrupt
+        elif len(user_input) == 1 and user_input.lower() in 'abcdef':
+            return user_input
+        else: 
+            user_input = pyip.inputChoice(['a','b','c', 'd', 'e', 'f'])
+            return user_input
+    except KeyboardInterrupt:
+        print("It was nice while it lasted. See you next time!")
+
+
+# assess user answer is between letters a - h
+def answer_actions_ah():
+    user_input = input()
+    try:
+        if user_input.lower() == 'quit':
+            raise KeyboardInterrupt
+        elif len(user_input) == 1 and user_input.lower() in 'abcdefgh':
+            return user_input
+        else: 
+            user_input = pyip.inputChoice(['a','b','c', 'd', 'e', 'f', 'g', 'h'])
+            return user_input
+    except KeyboardInterrupt:
+        print("It was nice while it lasted. See you next time!")
+
+
+# assess user's answer is between letters a - j
+def answer_actions_aj():
+    user_input = input()
+    try:
+        if user_input.lower() == 'quit':
+            raise KeyboardInterrupt
+        elif len(user_input) == 1 and user_input.lower() in 'abcdefghij':
+            return user_input
+        else: 
+            user_input = pyip.inputChoice(['a','b','c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'])
+            return user_input
+    except KeyboardInterrupt:
+        print("It was nice while it lasted. See you next time!")
      
 
 # First question and first set of possible answers
 def question_one_answers():
+    try:
     # Directions for the user
-    print("Please answer questions by entering the corresponding letter and pressing ENTER.")
-    (question_series(question_one.number, question_one.question, possible_answers[:7]))
-    # user_answer = answer_actions()
-    # return answer_actions
+        print("Please answer questions by typing the corresponding letter and pressing ENTER\nEnd the program without saving by typing QUIT and pressing ENTER")
+        (question_series(question_one.number, question_one.question, possible_answers[:7]))
+        answer_actions_ag()
+    except KeyboardInterrupt:
+        print("It was nice while it lasted! See you next time!")
 
-# Second question and possible answers
+
+# Second question and possible answers >> question 3 has become question 2
 def question_two_answers():
-    if answer_actions() in "abcdefghijk":
-        (question_series(question_two.number, question_two.question, possible_answers[7:18]))
-        return answer_actions
+    try: 
+        (question_series(question_two.number, question_two.question, possible_answers[18:29]))
+        answer = answer_actions_ak()
+        return answer
+    except KeyboardInterrupt:
+        print("It was nice while it lasted! See you next time!")
 
-
-# Third questions and possible answers
+# Question three and possible answers
 def question_three_answers():
-    if answer_actions() in "abcdefghijk":
-    # Third question and possible answers
-        (question_series(question_three.number, question_three.question, possible_answers[18:29]))
-        return answer_actions()
-
-# Question four and possible answers
-def question_four_answers():
-    match answer_actions():
+    match possible_user_answers:
         case 'a':
-            (question_series(question_four.number, question_four.question, possible_answers[29:32]))
-            return 'a'
+            try:
+                (question_series(question_three.number, question_three.question, possible_answers[29:32]))
+                answer_actions_ac()
+            except KeyboardInterrupt:
+                print("It was nice while it lasted! See you next time!")
         case 'b':
-            (question_series(question_four.number, question_four.question, possible_answers[31:35]))
-            return 'b'
+            try: 
+                (question_series(question_three.number, question_three.question, possible_answers[31:35]))
+                answer_actions_ad()
+            except KeyboardInterrupt:
+                print("It was nice while it lasted! See you next time!")
         case 'c':
-            (question_series(question_four.number, question_four.question, possible_answers[35:40]))
-            return 'c'
+            try:
+                (question_series(question_three.number, question_three.question, possible_answers[35:40]))
+                answer_actions_ae()
+            except KeyboardInterrupt:
+                print("It was nice while it lasted! See you next time!")
         case 'd':
-            (question_series(question_four.number, question_four.question, possible_answers[39:42]))
-            return 'd'
+            try:
+                (question_series(question_three.number, question_three.question, possible_answers[39:42]))
+                answer_actions_ac()
+            except KeyboardInterrupt:
+                print("It was nice while it lasted! See you next time!")
         case 'e':
-            (question_series(question_four.number, question_four.question, possible_answers[39:43]))
-            return 'e'
+            try:
+                (question_series(question_three.number, question_three.question, possible_answers[39:43]))
+                answer_actions_ad()
+            except KeyboardInterrupt:
+                print("It was nice while it lasted! See you next time!")
         case 'f':
-            (question_series(question_four.number, question_four.question, possible_answers[42:46]))
-            return 'f'
+            try:
+                (question_series(question_three.number, question_three.question, possible_answers[42:46]))
+                answer_actions_ad()
+            except KeyboardInterrupt:
+                print("It was nice while it lasted! See you next time!")
         case 'g':
-            (question_series(question_four.number, question_four.question, possible_answers[44:47]))
-            return 'g'
+            try:
+                (question_series(question_three.number, question_three.question, possible_answers[44:47]))
+                answer_actions_ac()
+            except KeyboardInterrupt:
+                print("It was nice while it lasted! See you next time!")
         case 'h':
-            (question_series(question_four.number, question_four.question, possible_answers[46:49]))
-            return 'h'
+            try:
+                (question_series(question_three.number, question_three.question, possible_answers[46:49]))
+                answer_actions_ac()
+            except KeyboardInterrupt:
+                print("It was nice while it lasted! See you next time!")
         case 'i':
-            (question_series(question_four.number, question_four.question, possible_answers[48:51]))
-            return 'i'
+            try:
+                (question_series(question_three.number, question_three.question, possible_answers[48:51]))
+                answer_actions_ac()
+            except KeyboardInterrupt:
+                print("It was nice while it lasted! See you next time!")
         case 'j':
-            (question_series(question_four.number, question_four.question, possible_answers[50:56]))
-            return 'j'
+            try:
+                (question_series(question_three.number, question_three.question, possible_answers[50:56]))
+                answer_actions_af()
+            except KeyboardInterrupt:
+                print("It was nice while it lasted! See you next time!")
         case 'k':
-            (question_series(question_four.number, question_four.question, possible_answers[53:56]))
-            return 'k'
+            try:
+                (question_series(question_three.number, question_three.question, possible_answers[53:56]))
+                answer_actions_ac()
+            except KeyboardInterrupt:
+                print("It was nice while it lasted! See you next time!")
+
+
+# Fourth question and possible answers
+def question_four_answers():
+    try: 
+        (question_series(question_four.number, question_four.question, possible_answers[7:18]))
+        answer = answer_actions_ak()
+        return answer
+    except KeyboardInterrupt:
+        print("It was nice while it lasted! See you next time!")
 
 
 # Question five - prints answer options based on the input taken in response to question_three_answers()
 def question_five_answers():
-    match question_three_answers():
+    match possible_user_answers:
         case 'a':
-            (question_series(question_five.number, question_five.question, possible_answers[56:64]))
+            try:
+                (question_series(question_five.number, question_five.question, possible_answers[56:64]))
+                answer_actions_ah()
+            except KeyboardInterrupt:
+                print("It was nice while it lasted! See you next time!")
         case 'b':
-            (question_series(question_five.number, question_five.question, possible_answers[64:74]))
+            try:
+                (question_series(question_five.number, question_five.question, possible_answers[64:74]))
+                answer_actions_aj()
+            except KeyboardInterrupt:
+                print("It was nice while it lasted! See you next time!")
         case 'c':
-            (question_series(question_five.number, question_five.question, possible_answers[74:84]))
+            try:
+                (question_series(question_five.number, question_five.question, possible_answers[74:84]))
+                answer_actions_aj()
+            except KeyboardInterrupt:
+                print("It was nice while it lasted! See you next time!")
         case 'd':
-            (question_series(question_five.number, question_five.question, possible_answers[84:94]))
+            try:
+                (question_series(question_five.number, question_five.question, possible_answers[84:94]))
+                answer_actions_aj()
+            except KeyboardInterrupt:
+                print("It was nice while it lasted! See you next time!")
         case 'e':
-            (question_series(question_five.number, question_five.question, possible_answers[94:104]))
+            try:
+                (question_series(question_five.number, question_five.question, possible_answers[94:104]))
+                answer_actions_aj()
+            except KeyboardInterrupt:
+                print("It was nice while it lasted! See you next time!")
         case'f':
-            (question_series(question_five.number, question_five.question, possible_answers[104:114]))
+            try:
+                (question_series(question_five.number, question_five.question, possible_answers[104:114]))
+                answer_actions_aj()
+            except KeyboardInterrupt:
+                print("It was nice while it lasted! See you next time!")
         case 'g':
-            (question_series(question_five.number, question_five.question, possible_answers[114:124]))
+            try:
+                (question_series(question_five.number, question_five.question, possible_answers[114:124]))
+                answer_actions_aj()
+            except KeyboardInterrupt:
+                print("It was nice while it lasted! See you next time!")
         case 'h':
-            (question_series(question_five.number, question_five.question, possible_answers[124:134]))
+            try:
+                (question_series(question_five.number, question_five.question, possible_answers[124:134]))
+                answer_actions_aj()
+            except KeyboardInterrupt:
+                print("It was nice while it lasted! See you next time!")
         case 'i':
-            (question_series(question_five.number, question_five.question, possible_answers[134:144]))
+            try:
+                (question_series(question_five.number, question_five.question, possible_answers[134:144]))
+                answer_actions_aj()
+            except KeyboardInterrupt:
+                print("It was nice while it lasted! See you next time!")
         case 'j':
-            (question_series(question_five.number, question_five.question, possible_answers[144:154]))
+            try:
+                (question_series(question_five.number, question_five.question, possible_answers[144:154]))
+                answer_actions_aj()
+            except KeyboardInterrupt:
+                print("It was nice while it lasted! See you next time!")
         case 'k':
-            (question_series(question_five.number, question_five.question, possible_answers[154:157]))
+            try:
+                (question_series(question_five.number, question_five.question, possible_answers[154:157]))
+                answer_actions_ac()
+            except KeyboardInterrupt:
+                print("It was nice while it lasted! See you next time!")
 
+# Clear terminal 
+clear()
 
+# Ask first queestion
 question_one_answers()
+clear()
 
-question_two_answers()
+# Ask second question
+possible_user_answers = question_two_answers()
+clear()
 
-# question_three_answers()
+# Ask third question
+question_three_answers()
+clear()
 
-question_four_answers()
+# Ask fourth question
+possible_user_answers = question_four_answers()
+clear()
 
+# Ask firth question
 question_five_answers()
-answer_actions()
-
-
-
-# with open('question.csv') as f:
-#         reader = csv.DictReader(f)
-#         birth_year = []
-#         for row in reader:
-#             birth_year.append(row['year_range'])
-
-
-
-
-
-
-# print(question_four.question)
- 
-# def ask_user_questions():
-
-
-# def question_one():
-#     # answer = input()
-#     for key in questions_dictionary.items():
-#         print(key)
-        
-        
-    # while answer != "quit":
-
-# question_one()

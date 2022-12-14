b7e46de6d150c4143e35e8a7b900293e19da0ba0
ConnieJacques <connie.jacques91@gmail.com>
2022-12-14 18:30:20 +1100

    result.py edited to attempt to correct output repitition bug

--- src/result.py
+++ src/result.py
@@ -10,16 +10,17 @@ import questions
 
 # Ask user for the month they were born in
 def get_month():
-    print("In what month were you born? Please enter a number between 1 - 12")
-    user_input = input()
+    # user_input = input("In what month were you born? Please enter a number between 1 - 12\n")
     try:
-        if user_input.lower() == 'quit':
-            raise KeyboardInterrupt
-        elif user_input in range(1, 12 + 1):
+        
+        # elif user_input in range(1, 12 + 1):
+            # return user_input
+        user_input = pyip.inputInt(prompt = "In what month were you born? Please enter a number between 1 - 12\n", min = 1, max = 12)
+        if user_input == True:
             return user_input
         else:
-            user_input = pyip.inputInt(min = 1, max = 12)
-            return user_input
+            if user_input.lower() == 'quit':
+                raise KeyboardInterrupt
     except KeyboardInterrupt:
         print("It was nice while it lasted. See you next time!")
         raise SystemExit
@@ -27,8 +28,7 @@ def get_month():
 
 # Ask user for the day they were born on
 def get_day():
-    print("On what day of the month were you born? Please enter a number between 1 - 31")
-    user_input = input()
+    user_input = input("On what day of the month were you born? Please enter a number between 1 - 31\n")
     try:
         if user_input.lower() == 'quit':
             raise KeyboardInterrupt
@@ -43,27 +43,30 @@ def get_day():
 
 
 def results():
+    clear()
     # Prompt user for month of birth and store in variable month
     month = get_month()
+    user_month = month
     # clear()
     # Prompt user for day of birth and store in variable day
     day = get_day()
+    user_day = day
     # Get today's date
     today = datetime.date.today()
     # get datetime object dfor users birthday
-    birthday = datetime.date(1991, month, day)
+    birthday = datetime.date(2008, user_month, user_day)
     # Calculate user's age
     age_year = rd(today, birthday)
     # Display result and append to results list global variable
-    your_age = print(f"You are {age_year.years} years old!")
+    your_age = print(f"You are {age_year.years} years old!\n")
     user_results.user_answer_list.append(age_year.years)
     # Display custom fact for user and append to results list
-    custom_fact = print(f"Fun fact for the year {1991}: {facts.dictionary.get(('1991'))}")
-    user_results.user_answer_list.append(custom_fact)
+    # custom_fact = print(f"Fun fact for the year {user_results.birth_year[0]}:\n{facts.dictionary.get((user_results.birth_year[0]))}")
+    # user_results.user_answer_list.append(custom_fact)
     # Get bonus random fact and append to results list
     random_fact = randfacts.get_fact()
-    display_random_fact = print(f"BONUS! enjoy this fun, completely random fact:\n{random_fact}")
-    user_results.user_answer_list.append(display_random_fact)
+    print(f"BONUS! enjoy this fun, completely random fact:\n{random_fact}")
+    return age_year
 
 # clear()
 results()
